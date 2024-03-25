33a401bf05c4338afb925872362480928bad9982
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2024-03-25 01:07:45 +0100

    refactor

--- 02-Plugins-Planners/Agents/BaseAgent.cs
+++ 02-Plugins-Planners/Agents/BaseAgent.cs
@@ -18,25 +18,26 @@ namespace DurableFunctions.SemanticKernel.Agents
             await SendMessage($"<br>{response}<br><br>");
             await SendMessage($"<hr><b>{GetType().Name} FINISHED</b><hr>");
 
-            await Task.WhenAll(s_agents.Select(a => a.DeleteAsync()));
+            //Clean up agents - Only needed for SemanticKernel native agents
+            await Task.WhenAll(_agents.Select(a => a.DeleteAsync()));
 
             return response;
         }
 
-        private static readonly List<IAgent> s_agents = new();
-
+        private static readonly List<IAgent> _agents = [];
         protected abstract Task<string?> ExecuteAgent(string input);
 
-        internal async Task SendMessage(string? message)
+        internal static async Task SendMessage(string? message)
         {
             if (message == null) return;
             await WebCliBridge.SendMessage(message);
         }
 
+        //For SemanicKernel native agents
+        //You need to keep track of the agents so you can clean them up when you're done
         internal static IAgent Track(IAgent agent)
         {
-            s_agents.Add(agent);
-
+            _agents.Add(agent);
             return agent;
         }
     }
